c15df7ee40e5b0dd54b9d23f1a5d13bdcbb7de0a
Matic Leva <vertical3life@gmail.com>
2018-10-24 16:55:21 +0200

    Attempt to detect holds with canny+contours

--- src/Processor.ts
+++ src/Processor.ts
@@ -2,6 +2,7 @@ import {VideoSource} from "./VideoSource";
 import * as WebSocket from "ws";
 import * as cv from "opencv4nodejs";
 import {DataPacket, FramePacket, SizePacket} from "./types";
+import * as path from "path";
 
 export class Processor {
     private camera: VideoSource;
@@ -40,16 +41,45 @@ export class Processor {
             port: parseInt(port),
         });
 
+        const blurMatrix: cv.Size = new cv.Size(3, 3);
+        // const scaleFactor = 0.5;
+        const red = new cv.Vec3(0, 0, 255);
+
         this.server.on('connection', ws => {
             const time = process.uptime().toString();
+            const filename = path.join(process.cwd(), 'assets/20180919_194756.mp4');
             this.clients[time] = ws;
             if (!this.camera)
-                this.camera = new VideoSource('../assets/20180919_194756.mp4');
+                this.camera = new VideoSource(filename);
             // this.camera = new VideoSource(0);
-            this.camera.on('frame', (frame: cv.Mat) => {
+            this.camera.on('frame', async (frame: cv.Mat) => {
                 // console.log(frame);
                 // cv.imshow('s-gym', frame);
-                this.sendFrame(frame);
+                try {
+                    // let original = frame.resize(this.camera.frameSize.height * scaleFactor, this.camera.frameSize.width * scaleFactor);
+                    let copy = frame
+                        .copy()
+                        .blur(blurMatrix)
+                        .bgrToGray()
+                        // .adaptiveThreshold(255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 7, 4)
+                        .canny(50, 100)
+                        // erode, then dilate
+                        .bitwiseNot()
+                    ;
+                    let contours = copy.findContours(cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
+                    contours.sort((a, b) => b.area - a.area);
+                    // copy = copy.cvtColor(cv.COLOR_GRAY2RGB);
+                    contours
+                        .slice(1, 300)
+                        .forEach(c => {
+                            frame.drawRectangle(c.boundingRect(), red);
+                        })
+                    ;
+                    this.sendFrame(frame);
+                }
+                catch (e) {
+                    debugger;
+                }
             });
             this.sendSize(this.camera.frameSize);
 
